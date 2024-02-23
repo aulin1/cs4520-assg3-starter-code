72fefc7cc2be05065d8887adc9a0cad264e7ebba
aulin1 <audrey.youran.lin@gmail.com>
2024-02-22 20:31:38 -0500

    View Binding

--- app/src/main/java/com/cs4520/assignment3/MVPFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/MVPFragment.kt
@@ -1,5 +1,9 @@
 package com.cs4520.assignment3
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class MVPFragment : Fragment(R.layout.mvp_fragment) {
